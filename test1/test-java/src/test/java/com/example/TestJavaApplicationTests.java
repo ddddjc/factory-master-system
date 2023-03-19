0d442260f2565b0931b408818f994c2fb1def557
djc <930657303@qq.com>
2023-03-19 19:27:02 +0800

    djc

--- test1/test-java/src/test/java/com/example/TestJavaApplicationTests.java
+++ test1/test-java/src/test/java/com/example/TestJavaApplicationTests.java
@@ -1,13 +1,10 @@
 package com.example;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TestJavaApplicationTests {
+    public static void main(String[] args) {
 
-    @Test
-    void contextLoads() {
     }
-
 }
