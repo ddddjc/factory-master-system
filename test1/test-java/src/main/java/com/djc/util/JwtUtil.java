6b6b829d1e1e9dfd164bc9dce2ee4a9b2a982f57
djc <930657303@qq.com>
2023-03-21 13:51:19 +0800

    djc

---
+++ test1/test-java/src/main/java/com/djc/util/JwtUtil.java
@@ -0,0 +1,59 @@
+package com.djc.util;
+
+import com.djc.entity.Employee;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    /**
+     * 生成 token
+     * @param employee token 包含的信息
+     * @return token
+     */
+    public String generateToken(Employee employee) {
+        Date expirationDate = new Date(System.currentTimeMillis() + expiration * 1000);
+        return Jwts.builder()
+                .claim("employeeId",employee.getEmployeeId())
+                .setExpiration(expirationDate)
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
+    }
+
+    /**
+     * 解析 token
+     * @param token token
+     * @return token 中包含的信息
+     */
+    public Claims parseToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    /**
+     * 从 token 中获取指定的值
+     * @param token token
+     * @param key 值的 key
+     * @param clazz 值的类型
+     * @return 指定的值
+     */
+    public <T> T getClaimFromToken(String token, String key, Class<T> clazz) {
+        Claims claims = parseToken(token);
+        Object value = claims.get(key);
+        return value == null ? null : clazz.cast(value);
+    }
+}
---
+++ test1/test-java/src/main/java/com/djc/util/SessionUtil.java
@@ -0,0 +1,45 @@
+package com.djc.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component // 将SessionUtil类标记为Spring Bean
+public class SessionUtil {
+
+    private final HttpServletRequest request; // 使用构造函数注入HttpServletRequest
+
+    @Autowired
+    public SessionUtil(HttpServletRequest request) { // 构造函数注入HttpServletRequest
+        this.request = request;
+    }
+
+    // 设置会话属性
+    public void setAttribute(String name, Object value) {
+        HttpSession session = request.getSession(); // 获取HttpSession
+        session.setAttribute(name, value); // 设置属性值
+    }
+
+    // 获取会话属性
+    public Object getAttribute(String name) {
+        HttpSession session = request.getSession(false); // 获取HttpSession（不创建新的）
+        return session != null ? session.getAttribute(name) : null; // 返回属性值或null
+    }
+
+    // 删除会话属性
+    public void removeAttribute(String name) {
+        HttpSession session = request.getSession(false); // 获取HttpSession（不创建新的）
+        if (session != null) {
+            session.removeAttribute(name); // 删除属性
+        }
+    }
+
+    // 使会话无效
+    public void invalidate() {
+        HttpSession session = request.getSession(false); // 获取HttpSession（不创建新的）
+        if (session != null) {
+            session.invalidate(); // 使会话无效
+        }
+    }
+}
